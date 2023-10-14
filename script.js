46a0add7b2a5c193c518661a23c68e4f1b5c21fb
artex137 <truthmonger01@gmail.com>
2023-10-14 00:41:27 -0400

    Added website files

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
-// Placeholder JavaScript file, as no specific functionality has been defined yet.
+// Select all image cards on the homepage
+const imageCards = document.querySelectorAll('.image-card');
+
+// Add hover event listener to each image card
+imageCards.forEach(card => {
+    card.addEventListener('mouseenter', showCaption);
+    card.addEventListener('mouseleave', hideCaption);
+});
+
+function showCaption(event) {
+    // Get the figcaption element within the hovered card
+    const caption = event.currentTarget.querySelector('figcaption');
+    
+    // Add a class to show the overlay (this class will be defined in CSS)
+    caption.classList.add('show-overlay');
+}
+
+function hideCaption(event) {
+    const caption = event.currentTarget.querySelector('figcaption');
+    caption.classList.remove('show-overlay');
+}
